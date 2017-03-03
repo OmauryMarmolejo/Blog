cddeafbb744bbeda5a5c893cc241ad7c54d0466a
OmauryMarmolejo <vmarmolejo@ucol.mx>
2017-03-02 22:46:24 -0600

    Comments

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,6 +6,8 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find(params[:id])
+    @comment = Comment.new
+    @comment.post_id = @post.id
   end
 
   def new
@@ -32,6 +34,8 @@ class PostsController < ApplicationController
   def update
     @post = Post.find(params[:id])
     @post.update(post_params)
+
+    flash.notice = "Post '#{@post.title}' updated"
 
     redirect_to post_path(@post)
   end
