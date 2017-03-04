04b3fa866e077c79ccfeef661551257ec16a87c0
OmauryMarmolejo <vmarmolejo@ucol.mx>
2017-03-03 19:18:09 -0600

    comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,21 @@
 class CommentsController < ApplicationController
   def create
-    @comment = Comment.new(comment_params)
+    @post = Post.find(params[:id])
+    @comment = @post.comments.build(params[:comment])
+    @comment.post = @post
+    @post.user = current_user
+    if @comment.save
+      redirect_to post_path(@comment.post)
+    else
+      render 'new'
+    end
+    @comment = current_user.comments.new
     @comment.post_id = params[:post_id]
     @comment.save
     redirect_to post_path(@comment.post)
   end
 
   def comment_params
-    params.require(:comment).permit(:author_name, :content)
+    params.require(:comment).permit(:user_id, :post_id,  :content)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170303154809) do
+ActiveRecord::Schema.define(version: 20170303232251) do
 
   create_table "comments", force: :cascade do |t|
     t.string "author_name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20170303154809) do
     t.integer "post_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t. "user"
     t.index ["post_id"], name: "index_comments_on_post_id"
   end
 
