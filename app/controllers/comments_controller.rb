3a781d5ad6d46e510a17f04ee8b69daeaf2ce739
OmauryMarmolejo <vmarmolejo@ucol.mx>
2017-03-07 11:17:56 -0600

    comments and login style

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,16 +2,15 @@ class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.build(comment_params)
-    @comment.post = @post
     @comment.user = current_user
     if @comment.save
-      redirect_to post_path(@comment.post)
+    redirect_to post_path(@comment.post)
     else
       render to post_path
     end
   end
 
   def comment_params
-    params.require(:comment).permit(:user_id)
+    params.require(:comment).permit(:user_id, :content)
   end
 end
