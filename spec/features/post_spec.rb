8c1b9f5ceb07971d66924293ae2ef223c9072b19
OmauryMarmolejo <vmarmolejo@ucol.mx>
2017-03-17 23:33:15 -0600

    New test

---
+++ spec/features/post_spec.rb
@@ -0,0 +1,22 @@
+#require 'spec_helper'
+
+#describe Post do
+  #describe "creation" do
+    #context "valid attributes" do
+      #it "should be valid" do
+        #sign_up FactoryGirl.create(:user)
+        #visit new_post_path
+        #post = FactoryGirl.build(:post)
+        #expect(post).to be_valid
+      #end
+    #end
+
+    #context "invalid attributes" do
+      #it "should not be valid" do
+        #sign_up FactoryGirl.create(:user)
+        #visit new_post_path
+        #expect(post).to be_valid
+      #end
+    #end
+  #end
+#end
